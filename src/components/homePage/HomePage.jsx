881a909652f473d2274ebe9323ca928bd026a7d8
guigzzzzz <guillaume.trouche@symbioz.io>
2020-10-20 16:34:22 +0200

    init homepage

---
+++ src/components/homePage/HomePage.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+import Navigation from '../nav/Navigation';
+
+function HomePage() {
+    return(
+        <div>
+            <Navigation />
+            <p>Bonjour voici la page d'acceuil bitch </p>
+        </div>
+    )
+}
+
+export default HomePage;
---
+++ src/components/nav/Navigation.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export default function Navigation() {
+    return(
+        <div>
+            <div>
+                Home
+            </div>
+            <div>
+                Gallery
+            </div>
+            <div>
+                Contact
+            </div>
+        </div>
+    )
+}
+
