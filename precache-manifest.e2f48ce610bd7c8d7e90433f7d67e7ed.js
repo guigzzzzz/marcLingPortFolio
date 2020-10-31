c56b10c8c37845177231e7c435bf9839915f64bc
Stephen <stephen@nr2.io>
2020-10-31 18:26:41 +0100

    Updates

---
+++ precache-manifest.e2f48ce610bd7c8d7e90433f7d67e7ed.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc9a19609fc3f79d8bc22636d9310943",
+    "url": "/marcLingPortFolio/index.html"
+  },
+  {
+    "revision": "8e2e66bec3478597c9f8",
+    "url": "/marcLingPortFolio/static/css/main.4ea2e134.chunk.css"
+  },
+  {
+    "revision": "3417608265a482e44dfd",
+    "url": "/marcLingPortFolio/static/js/2.c5f7e3a8.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/marcLingPortFolio/static/js/2.c5f7e3a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e2e66bec3478597c9f8",
+    "url": "/marcLingPortFolio/static/js/main.319b423f.chunk.js"
+  },
+  {
+    "revision": "489b475a16c497304ff9",
+    "url": "/marcLingPortFolio/static/js/runtime-main.f630245b.js"
+  },
+  {
+    "revision": "246ab53692f03baa96f6098eef15dd9b",
+    "url": "/marcLingPortFolio/static/media/frieza.246ab536.png"
+  },
+  {
+    "revision": "0b2ed311e439c0616d365323bdeb79ff",
+    "url": "/marcLingPortFolio/static/media/icon_globe.0b2ed311.png"
+  }
+]);
